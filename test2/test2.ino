5ca82eef9a2a04245786740ee8f77bc1d4f94fb2
Luca <sanluca78@gmail.com>
2025-07-14 08:42:29 +0200

    modifiche varie

--- test2/test2.ino
+++ test2/test2.ino
@@ -1,27 +1,29 @@
 //************************************************************
-// lettura presenza nel letto con HX711
-// wemos d1 mini 
-// painlessmesh 1.5.3
-// esp8266 3.1.2
-// arduinojson 6.21.5
+// read temperature and humidity "camera"
+// ESP32C3 Dev Module
+// rele  4
+// pulsante 0
+// Si7005
+//painlessmesh 1.5.3 arduinojson 6.21.5 esp32 3.0.7
 //************************************************************
-#include <HX711.h>
-// HX711 Pins
-const int LOADCELL_DOUT_PIN = 2; // D4 Remember these are ESP GPIO pins, they are not the physical pins on the board.
-const int LOADCELL_SCK_PIN = 3; //RX
-int calibration_factor = 400; // Defines calibration factor we'll use for calibrating. se diminuisce aumenta il valore inversamente proporzionale
-float reading; // Float for reading
-float raw; // Float for raw value which can be useful
+#include "Button2.h"
+#include <Wire.h>
+#include <Si7005.h>
+const int Si7005_CS_PIN = 1;
+Si7005 Si(Si7005_CS_PIN);
+float h,t;
 const unsigned long RETRY_ROOT_INTERVAL = 1200000; // 20 minuti
 bool retryTaskEnabled = false; // Flag per il task di retry
-// Variabili e costanti per la gestione errori HX711 e riavvio
-int hx711_error_count = 0; // Contatore errori consecutivi HX711
-const int MAX_HX711_ERRORS = 5; // Numero massimo di errori HX711 prima del riavvio
 
-HX711 scale;                          // Initiate HX711 library
+#define BUTTON_1        0
 
+int relay = 4;
+bool relayState = LOW;
+
+//Button2 button1;
+Button2 btn1(BUTTON_1);
 //#include "painlessMesh.h"
-#include "namedMesh.h"
+#include "../namedmesh/namedMesh.h"
 
 #define   MESH_PREFIX     "whateverYouLike"
 #define   MESH_PASSWORD   "somethingSneaky"
@@ -31,13 +33,13 @@ Scheduler userScheduler; // to control your personal task
 //painlessMesh  mesh;
 namedMesh  mesh;
 String msg,rel;
-String nodeName = "lettocamera"; // Name needs to be unique
+String nodeName = "test"; // Name needs to be unique
 String to = "bridgemqtt";
 uint32_t root_id=0;
 
-#define ROLE    "lettocamera"
-#define VERSION "LettoCamera v3.1.0"
-#define MESSAGE "lettocamera "
+#define ROLE    "test"
+#define VERSION "Test v1.0.1"
+#define MESSAGE "test "
 
 // User stub
 void sendMessage() ; // Prototype so PlatformIO doesn't complain
@@ -49,31 +51,38 @@ Task taskSendMessage1( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage1 );
 Task retryRootTask(RETRY_ROOT_INTERVAL, TASK_ONCE, &retryRoot);
 
 void sendMessage() {
-  read_peso();
+  read_SI7005();
+  msg = "temperatura/";
+  msg += t;
+  mesh.sendSingle(to, msg);
+  msg = "umidita/";
+  msg += h;
+  mesh.sendSingle(to, msg);
   taskSendMessage.setInterval( random( TASK_SECOND * 300, TASK_SECOND * 400 ));
 }
 
 void sendMessage1() {
   update_status();
-  taskSendMessage1.setInterval( random( TASK_SECOND * 600, TASK_SECOND * 700 ));
+  taskSendMessage1.setInterval( random( TASK_SECOND * 450, TASK_SECOND * 550 ));
 }
 
 // Needed for painless library
 void receivedCallback( uint32_t from, String &msg ) {
   Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
-  root_id=from;
   rel=msg;
-
+  root_id=from;
     if (strcmp(rel.c_str(),"1") == 0){
-      //tara la bilancia
-      //scale.power_up();
-      //scale.wait_ready();
-      if (scale.is_ready())
-      {
-      scale.set_scale(calibration_factor);
-      scale.tare();       //Reset scale to zero
-      //scale.power_down();
-  }}}
+      digitalWrite(relay, HIGH);
+      relayState = HIGH;
+      msg = "output/1";
+      mesh.sendSingle(to, msg);
+    }
+    else if (strcmp(rel.c_str(),"0") == 0) {
+      digitalWrite(relay, LOW);
+      relayState = LOW;
+      msg = "output/0";
+      mesh.sendSingle(to, msg);
+  }}
 
 void newConnectionCallback(uint32_t nodeId) {
     Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
@@ -91,53 +100,39 @@ void changedConnectionCallback() {
     Serial.println("Connessione al root ripristinata.");
     retryRootTask.disable();
     retryTaskEnabled = false; // Resetta il flag
-  }}
+}}
 
 void nodeTimeAdjustedCallback(int32_t offset) {
     Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
 }
 
-void read_peso() {
-  scale.power_up(); // Accendi l'HX711
-  if (scale.wait_ready_timeout(5000)) { // Attendi che la scala sia pronta con timeout
-    hx711_error_count = 0;
-    scale.set_scale(calibration_factor); // Imposta il fattore di calibrazione
-    reading = scale.get_units(10); // Leggi la scala in g/Kg
-    raw = scale.read_average(5); // Leggi il valore raw dalla scala
+//void button_setup(Button2& btn){
+void button_setup() {
+  btn1.setPressedHandler([](Button2 & b) {
+     // if (btn == button1){
+      relayState = !relayState;
+      digitalWrite(relay,relayState);
+    if (relayState == HIGH){
+      msg = "output/1";
+      mesh.sendSingle(to, msg);
+     }
+    else if (relayState == LOW){
+      msg = "output/0";
+      mesh.sendSingle(to, msg);
+     }});}
 
-    if (reading < 0) {
-      reading = 0.00; // Imposta la lettura a 0 se è negativa
+void read_SI7005() {
+  if (Si.detectSensor()) {
+    h = Si.getHumidity();
+    t = Si.getTemperature();
     }
+  else  {
+      msg = "error/Failed to read from SI7005 sensor!";
+      mesh.sendSingle(to, msg);
+      return;
+    }}
 
-    // Usa snprintf per formattare le stringhe in modo più efficiente
-    char reading_str[20];
-    char raw_str[20];
-    snprintf(reading_str, sizeof(reading_str), "%.2f", reading); // Formatta con 2 decimali
-    snprintf(raw_str, sizeof(raw_str), "%.0f", raw); // Formatta senza decimali
-
-    msg = "peso/";
-    msg += reading_str;
-    mesh.sendSingle(to, msg);
-
-    msg = "valuerawstr/";
-    msg += raw_str;
-    mesh.sendSingle(to, msg);
-  } else {
-    hx711_error_count++;
-    Serial.println("Errore: HX711 non pronto!");
-    msg = "error/HX711notready";
-    mesh.sendSingle(to, msg);
-    if (hx711_error_count >= MAX_HX711_ERRORS) {
-      Serial.printf("Raggiunto il limite di %d errori consecutivi dell'HX711. Riavvio in corso...\n", MAX_HX711_ERRORS);
-      delay(1000); // Piccolo ritardo per permettere la stampa seriale
-      ESP.restart(); // Riavvia la scheda
-    }
-  }
-  scale.power_down(); // Spegni l'HX711
-}
-
-void update_status()
-{
+void update_status() {
   long uptime = millis() / 60000L;
   msg = "uptime/";
   msg += uptime;
@@ -169,22 +164,18 @@ void retryRoot() {
   } else {
     Serial.println("Root tornato online.");
     retryTaskEnabled = false; // Resetta il flag anche qui, per sicurezza
-  }
-}
+  }}
 
 void setup() {
   Serial.begin(115200);
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);   // Start scale on specified pins
-  //scale.wait_ready();                                 //Ensure scale is ready, this is a blocking function
-  scale.set_scale(calibration_factor);                                  
-  //scale.wait_ready();
-  //scale.tare();                                       // Tare scale on startup
-  //scale.wait_ready();
+  Wire.begin();
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay,LOW);
 
   //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_AP_STA, 11 );
 
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_AP_STA, 11 );
   mesh.initOTAReceive(ROLE);
   mesh.setContainsRoot(true);
   mesh.setName(nodeName);
@@ -197,9 +188,15 @@ void setup() {
   userScheduler.addTask( taskSendMessage1 );
   taskSendMessage.enable();
   taskSendMessage1.enable();
+
+  //button1.begin(BUTTON_1);
+  //button1.setClickHandler(button_setup);
+  button_setup();
 }
 
 void loop() {
   // it will run the user scheduler as well
   mesh.update();
+  //button1.loop();
+  btn1.loop();
 }
